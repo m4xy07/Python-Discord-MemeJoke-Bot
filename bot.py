b3c2b2d6a4138d02df2505e6a552461660d3ee87
Aman S <hey@m4xy.org>
2024-05-12 14:34:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import requests
 
 intents = discord.Intents.default()
-intents.message_content = True  # Needed for commands
+intents.message_content = True 
 
 client = commands.Bot(intents=intents, command_prefix='!')
 
